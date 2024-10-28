f301542c4134ee4b138e5fd72d597537374ffba5
johannes <johannestoke@gmail.com>
2024-10-28 11:21:32 +0100

    added lots of nice things

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -12,16 +12,12 @@ config.default_prog = { 'nu' }
 
 config.font = wezterm.font 'JetBrainsMono NF'
 config.font_size = 12
-config.window_background_opacity = 1
+config.window_background_opacity = 0.8
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.use_fancy_tab_bar = false
 config.color_scheme = 'One Dark (Gogh)'
 config.hide_tab_bar_if_only_one_tab = true
 
-
-  
-
 -- and finally, return the configuration to wezterm
-
 
 return config
