39baa7b5c6c7eb7129ec039cd40df1c2d5e7c3cd
johannes <johannestoke@gmail.com>
2025-03-03 00:02:48 +0100

    removing old stuff

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-New-Alias sudo gsudo
-
-# Microsoft.PowerShell_profile.ps1
-$Env:KOMOREBI_CONFIG_HOME = "$($Env:USERPROFILE)\.config\komorebi"
-
--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,2 +0,0 @@
-# Microsoft.PowerShell_profile.ps1
-$Env:KOMOREBI_CONFIG_HOME = "$($Env:USERPROFILE)\.config\komorebi"
